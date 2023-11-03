17046792f311305ad2a2d1204b629388cb888268
Kazi Robin <kazirobinking@gmail.com>
2023-11-03 11:25:25 +0600

    update js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// navbar menu section 
+const navMenu = document.querySelector('.navbar_menu');
+const navBar = document.querySelector('.navbar_bar');
+navBar.addEventListener('click',() => {
+    navMenu.classList.toggle('active')
+    navBar.classList.toggle('active')
+})
